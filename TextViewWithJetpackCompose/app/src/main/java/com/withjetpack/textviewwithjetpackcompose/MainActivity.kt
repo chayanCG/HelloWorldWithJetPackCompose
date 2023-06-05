cf032d6db80e8743f1150139ef7635befbd0decc
chayan chowdhury <chayanchowdhury43@yahoo.com>
2023-06-05 22:03:56 +0530

    Initial commit

--- TextViewWithJetpackCompose/app/src/main/java/com/withjetpack/textviewwithjetpackcompose/MainActivity.kt
+++ TextViewWithJetpackCompose/app/src/main/java/com/withjetpack/textviewwithjetpackcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.withjetpack.textviewwithjetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -23,6 +24,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -53,12 +55,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun sampleText(name: String) {
+    var context= LocalContext.current
     Text(
         text = "Hello $name!",
         style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
         modifier = Modifier.padding(20.dp).background(Color.Gray, shape = RoundedCornerShape(10.dp))
             .padding(20.dp).clickable {
-
+                Toast.makeText(context,"CLicked here",Toast.LENGTH_LONG).show()
             }
     )
 }
